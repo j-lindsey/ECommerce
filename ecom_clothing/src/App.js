e90965f006a208a00131ea3c3d22016a653e2756
j-lindsey <j-lindsey@users.noreply.github.com>
2021-02-04 08:53:12 -0700

    added form input component

--- ecom_clothing/src/App.js
+++ ecom_clothing/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-out.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
