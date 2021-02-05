0a5a91a6edb344c70c9a0bfd5f5a0ae82f91888d
j-lindsey <j-lindsey@users.noreply.github.com>
2021-02-05 15:41:57 -0700

    started firebase addition to project

---
+++ ecom_clothing/src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+const Config = {
+    apiKey: "AIzaSyBVJnRqbwZ-LHZpz_gHCkraGvqnUQF7Qhg",
+    authDomain: "ecommerce-2615a.firebaseapp.com",
+    projectId: "ecommerce-2615a",
+    storageBucket: "ecommerce-2615a.appspot.com",
+    messagingSenderId: "211645714803",
+    appId: "1:211645714803:web:d848f8a7b5e92f6cecf506",
+    measurementId: "G-B7HP76FYPW"
+};
+
+firebase.initializeApp(Config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = ()=> auth.signInWithPopup(provider);
+
+export default firebase;
