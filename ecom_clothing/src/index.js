c39a77994daa7411bb458544d87c4fa3da302587
j-lindsey <j-lindsey@users.noreply.github.com>
2021-02-13 17:16:28 -0700

    setting up redux

--- ecom_clothing/src/index.js
+++ ecom_clothing/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import store from './redux/store';
 
 import './index.css';
 import App from './App';
@@ -10,7 +11,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
